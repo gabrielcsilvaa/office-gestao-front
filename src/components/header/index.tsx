f6b314dcbe003167e7981690465ca811af228be6
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-07 16:10:15 -0300

    Left-menu e header concluido

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,43 @@
 import Link from "next/link";
 import "../../app/globals.css";
 import { Cairo } from "next/font/google";
+import Image from "next/image";
 
 // Defina a fonte como uma variável
 const cairo = Cairo({
-  weight: ["700", "700"], // Você pode especificar os pesos que deseja (normal e negrito)
-  subsets: ["latin"], // Se você precisa de algum subconjunto específico (como latino)
+  weight: ["500", "600", "700"], // Você pode especificar os pesos que deseja (normal e negrito)
+  subsets: ["latin"],
 });
 
 export function Header() {
   return (
     <header
-      className={`fixed lg:relative z-40 w-[255px] h-full flex flex-col gap-2 px-2 py-4 bg-[var(--left-menu-gray)]`}
+      className={`text-white fixed lg:relative z-40 w-[255px] h-full flex flex-col gap-2 px-2 py-4 bg-[var(--left-menu-gray)]`}
     >
-      <div className="text-xl font-bold mb-6">Meu Menu</div>
+      <Image
+        src="/assets/logos/office.svg"
+        alt="Ícone"
+        width={220}
+        height={84}
+      />
       {/* Adicione os itens de navegação ou outros componentes */}
-      <nav className={`${cairo.className}`}>
+      <nav className={`mt-6 text-[17px] ${cairo.className}`}>
         <ul>
           <li className="mb-4">
             <Link
               href="/modulos/gestao/carteira"
               className="hover:text-gray-400"
             >
-              Análise Carteira
+              <span className="inline-flex items-center text-lg">
+                <Image
+                  src="/assets/icons/Frame Carteira.svg"
+                  alt="Ícone"
+                  width={22}
+                  height={22}
+                  className="mr-2" // margem à esquerda para espaçamento entre o texto e o ícone
+                />
+                Análise de Carteira
+              </span>
             </Link>
           </li>
           <li className="mb-4">
@@ -30,7 +45,16 @@ export function Header() {
               href="/modulos/gestao/clientes"
               className="hover:text-gray-400"
             >
-              Análise Clientes
+              <span className="inline-flex items-center text-lg">
+                <Image
+                  src="/assets/icons/Frame Clientes.svg"
+                  alt="Ícone"
+                  width={22}
+                  height={22}
+                  className="mr-2" // margem à esquerda para espaçamento entre o texto e o ícone
+                />
+                Análise de Clientes
+              </span>
             </Link>
           </li>
           <li className="mb-4">
@@ -38,7 +62,16 @@ export function Header() {
               href="/modulos/gestao/usuarios"
               className="hover:text-gray-400"
             >
-              Análise Usuários
+              <span className="inline-flex items-center text-lg">
+                <Image
+                  src="/assets/icons/Frame 28.svg"
+                  alt="Ícone"
+                  width={22}
+                  height={22}
+                  className="mr-2" // margem à esquerda para espaçamento entre o texto e o ícone
+                />
+                Análise de Usuários
+              </span>
             </Link>
           </li>
           <li className="mb-4">
@@ -46,10 +79,60 @@ export function Header() {
               href="/modulos/gestao/escritorio"
               className="hover:text-gray-400"
             >
-              Análise Escritório
+              <span className="inline-flex items-center text-lg">
+                <Image
+                  src="/assets/icons/Frame Escritorio.svg"
+                  alt="Ícone"
+                  width={22}
+                  height={22}
+                  className="mr-2" // margem à esquerda para espaçamento entre o texto e o ícone
+                />
+                Análise de Escritório
+              </span>
             </Link>
           </li>
         </ul>
+      </nav>
+    </header>
+  );
+}
+
+export function Header2() {
+  return (
+    <header className="flex items-center p-2 h-16 bg-white border-[1px] border-[#E5E5E5]">
+      <nav className="flex w-full justify-between items-center">
+        <span
+          className={`${cairo.className} font-[700] text-[32px] text-[var(--left-menu-gray)]`}
+        >
+          Módulo Gestão
+        </span>
+
+        <div className="flex gap-2 ml-auto">
+          <Image
+            src="/assets/icons/Frame 33.svg"
+            alt="Ícone"
+            width={40}
+            height={40}
+          />
+          <Image
+            src="/assets/icons/Frame 34.svg"
+            alt="Ícone"
+            width={40}
+            height={40}
+          />
+          <Image
+            src="/assets/icons/Frame 35.svg"
+            alt="Ícone"
+            width={40}
+            height={40}
+          />
+          <Image
+            src="/assets/icons/Frame 36.svg"
+            alt="Ícone"
+            width={40}
+            height={40}
+          />
+        </div>
       </nav>
     </header>
   );
