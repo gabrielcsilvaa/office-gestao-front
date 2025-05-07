6a6087a4371c27c60edf5a5d68efcc6615cdc3d4
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-07 14:48:58 -0300

    font cairo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
+import "../../app/globals.css";
+import { Cairo } from "next/font/google";
+
+// Defina a fonte como uma variável
+const cairo = Cairo({
+  weight: ["700", "700"], // Você pode especificar os pesos que deseja (normal e negrito)
+  subsets: ["latin"], // Se você precisa de algum subconjunto específico (como latino)
+});
 
 export function Header() {
   return (
-    <header className="fixed lg:relative z-40 w-[255px] h-full flex flex-col gap-2 px-2 py-4 bg-[var(--left-menu-gray)]">
+    <header
+      className={`fixed lg:relative z-40 w-[255px] h-full flex flex-col gap-2 px-2 py-4 bg-[var(--left-menu-gray)]`}
+    >
       <div className="text-xl font-bold mb-6">Meu Menu</div>
       {/* Adicione os itens de navegação ou outros componentes */}
-      <nav>
+      <nav className={`${cairo.className}`}>
         <ul>
           <li className="mb-4">
             <Link
