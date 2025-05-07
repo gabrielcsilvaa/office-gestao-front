9f6396ddc0a234034b8e86ba8c0d0df15a16bbc3
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-07 14:29:03 -0300

    Criação de primeiras rotas e cor cinza lateral

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,29 +1,43 @@
+import Link from "next/link";
+
 export function Header() {
   return (
-    <header className="w-64 bg-gray-800 text-white p-4 h-full">
+    <header className="fixed lg:relative z-40 w-[255px] h-full flex flex-col gap-2 px-2 py-4 bg-[var(--left-menu-gray)]">
       <div className="text-xl font-bold mb-6">Meu Menu</div>
       {/* Adicione os itens de navegação ou outros componentes */}
       <nav>
         <ul>
           <li className="mb-4">
-            <a href="#" className="hover:text-gray-400">
-              Início
-            </a>
+            <Link
+              href="/modulos/gestao/carteira"
+              className="hover:text-gray-400"
+            >
+              Análise Carteira
+            </Link>
           </li>
           <li className="mb-4">
-            <a href="#" className="hover:text-gray-400">
-              Clientes
-            </a>
+            <Link
+              href="/modulos/gestao/clientes"
+              className="hover:text-gray-400"
+            >
+              Análise Clientes
+            </Link>
           </li>
           <li className="mb-4">
-            <a href="#" className="hover:text-gray-400">
-              Relatórios
-            </a>
+            <Link
+              href="/modulos/gestao/usuarios"
+              className="hover:text-gray-400"
+            >
+              Análise Usuários
+            </Link>
           </li>
           <li className="mb-4">
-            <a href="#" className="hover:text-gray-400">
-              Configurações
-            </a>
+            <Link
+              href="/modulos/gestao/escritorio"
+              className="hover:text-gray-400"
+            >
+              Análise Escritório
+            </Link>
           </li>
         </ul>
       </nav>
