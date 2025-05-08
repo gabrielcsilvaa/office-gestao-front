2f337e2236c4ccaaeb5fabae01e9e6a551724805
Gabriel <gabrielcardososilva290@gmail.com>
2025-05-08 16:44:58 -0300

    Empresa por Ramo de Atividade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,25 @@
 import "./globals.css";
 import { Header, Header2 } from "../components/header/index";
+import { Cairo } from "next/font/google";
+
+// Configurando a fonte Cairo
+const cairo = Cairo({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"], // Defina os pesos que você vai usar
+  variable: "--font-cairo", // Define uma variável CSS para Tailwind
+});
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>
+    <html lang="pt-br">
+      <head>
+        <style>{`:root { font-family: var(--font-cairo), sans-serif; }`}</style>
+      </head>
+      <body className={`antialiased ${cairo.variable} bg-gray-100`}>
         <div className="flex h-screen">
           <div>
             <Header />
