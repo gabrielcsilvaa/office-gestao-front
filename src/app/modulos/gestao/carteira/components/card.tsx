21acc304f8183892764405803eb203d7b90ab42c
Gabriel <gabrielcardososilva290@gmail.com>
2025-05-09 10:55:37 -0300

    modal dos cards feitos

--- src/app/modulos/gestao/carteira/components/card.tsx
+++ src/app/modulos/gestao/carteira/components/card.tsx
@@ -12,11 +12,15 @@ interface CardProps {
   title: string;
   value: number;
   icon: string;
+  onClick?: () => void;
+
 }
 
-export default function Card({ title, value, icon }: CardProps) {
+export default function Card({ title, value, icon, onClick }: CardProps) {
   return (
-    <div className="cursor-pointer py-2 px-4 flex flex-row justify-between items-center w-full h-20 bg-white rounded-md shadow-md">
+    <div className="cursor-pointer py-2 px-4 flex flex-row justify-between items-center w-full h-20 bg-white rounded-md shadow-md"
+    onClick={onClick}
+    >
       <div className="py-2 flex flex-col items-start justify-between">
         <span className={`text-base  ${cairo.className} text-left text-gray-500`}>{title}</span>
         <span className={`text-xl  ${cairo.className} block`}>{value}</span>
