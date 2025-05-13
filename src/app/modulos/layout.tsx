7c782b87985d18a05109f8b81e822393e539c3d8
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-13 18:34:36 -0300

    Corrigindo estrutura para layout homepage

---
+++ src/app/modulos/layout.tsx
@@ -0,0 +1,24 @@
+import "../globals.css";
+import { Header, Header2 } from "../../components/header/index";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={`antialiased`}>
+        <div className="flex h-screen">
+          <div>
+            <Header />
+          </div>
+          <div className="w-full">
+            <Header2 />
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
