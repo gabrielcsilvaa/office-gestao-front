19d4dbec9f4468745698ab2b8bae84e6af982110
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-13 19:21:07 -0300

    Pagina de login concluida e funcional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,50 @@ export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // Função que realiza a autenticação
+  const fetchLoginAuth = async () => {
+    try {
+      const body = {
+        user: email, // Usando email do formulário
+        password: password, // Usando senha do formulário
+      };
+
+      // Fazendo a requisição de login
+      const response = await fetch("/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erro na API: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      // Verificando a resposta da API
+      if (data.result === true) {
+        // Login bem-sucedido, armazenando token no localStorage
+        localStorage.setItem("token", "fake-jwt-token"); // Substitua por um token real, se necessário
+
+        // Redirecionando para a página principal
+        router.push("/modulos/gestao/carteira");
+      } else {
+        // Login falhou, exibe erro
+        setError("Credenciais inválidas.");
+      }
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message); // Exibindo a mensagem de erro
+      } else {
+        setError("Erro desconhecido");
+      }
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-r from-[#e2e2e2] to-[#c9d6ff] px-4">
@@ -20,7 +64,8 @@ export default function Login() {
           <span className="text-sm font-medium font-cairo text-black text-center w-full">
             utilize seu email e senha para acessar a plataforma
           </span>
-
+          {error && <div className="text-red-500">{error}</div>}{" "}
+          {/* Exibe erros */}
           <input
             type="email"
             value={email}
@@ -35,14 +80,15 @@ export default function Login() {
             className="w-full rounded-lg bg-[#F0F8FF] h-12 px-4 py-2 placeholder:text-[#373A4080]"
             placeholder="Senha"
           />
-
           <a href="" className="w-full text-center">
             <span className="text-sm font-medium font-cairo text-black">
               Esqueceu a senha?
             </span>
           </a>
-
-          <button className="w-40 rounded-lg bg-[#373A40] h-9 text-sm text-white font-cairo">
+          <button
+            onClick={fetchLoginAuth} // Chama a função de login ao clicar
+            className="w-40 rounded-lg bg-[#373A40] h-9 text-sm text-white font-cairo"
+          >
             Entrar
           </button>
         </div>
