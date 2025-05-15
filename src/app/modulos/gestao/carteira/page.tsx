ffa77e07c7f87ba6b80744053f46f55efbdaa1b0
Gabriel <gabrielcardososilva290@gmail.com>
2025-05-15 08:11:07 -0300

    tipagem dados novos

--- src/app/modulos/gestao/carteira/page.tsx
+++ src/app/modulos/gestao/carteira/page.tsx
@@ -58,6 +58,20 @@ interface Socio {
   idade?: number; // Tornando idade opcional já que vamos calculá-la
 }
 
+interface EmpresaCompletaNovosCliente {
+  id: number;
+  nome_empresa: string;
+  situacao: string;
+  cnpj: string;
+  data_cadastro: string;
+  responsavel_legal: string;
+  data_inatividade: string | null;
+  motivo_inatividade: number;
+  regime_tributario: string;
+}
+
+
+
 export default function Carteira() {
   const [selectedOption, setSelectedOption] =
     useState<string>("Selecionar Todos");
@@ -66,7 +80,7 @@ export default function Carteira() {
   const [socios, setSocios] = useState<Socio[]>([]); // Corrigido a tipagem
   const [parceria, setParceria] = useState<Parceria[]>([]); // Corrigido a tipagem
   const [novosClientes, setNovosClientes] = useState<number>(0);
-  const [dadosTotaisClientesNovos, setDadosTotaisClientesNovos] = useState<Empresa[]>([]);
+  const [dadosTotaisClientesNovos, setDadosTotaisClientesNovos] = useState<EmpresaCompletaNovosCliente[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [regimesData, setRegimesData] = useState<Regime[]>([]);
@@ -154,17 +168,16 @@ useEffect(() => {
       const data = await response.json();
 
       // Garantindo que os dados estão no formato correto
-      const dadosTotaisClientesNovos = data.flatMap(
-        (item: { empresas: any[] }) =>
-          item.empresas.map((empresa) => ({
-            nome_empresa: empresa.nome_empresa,
-            cnpj: empresa.cnpj,
-            data_cadastro: empresa.data_cadastro,
-            situacao: empresa.situacao,
-            responsavel_legal: empresa.responsavel || "Sem Responsável",
-            motivo_inatividade: empresa.motivo_inatividade || 0,
-            regime_tributario: empresa.regime_tributario || "Não Especificado",
-          }))
+      const dadosTotaisClientesNovos = data.flatMap((item: { empresas: EmpresaCompletaNovosCliente[] }) =>
+        item.empresas.map((empresa) => ({
+          nome_empresa: empresa.nome_empresa,
+          cnpj: empresa.cnpj,
+          data_cadastro: empresa.data_cadastro,
+          situacao: empresa.situacao,
+          responsavel_legal: empresa.responsavel_legal || "Sem Responsável",
+          motivo_inatividade: empresa.motivo_inatividade || 0,
+          regime_tributario: empresa.regime_tributario || "Não Especificado",
+        }))
       );
 
       const totalNovosClientes = dadosTotaisClientesNovos.length;
