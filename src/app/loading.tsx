ee307b751f92c7b9b7e3afe4c126e24a132e5697
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-21 10:05:17 -0300

    Aplicando tela de loading em tudo

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Reload from "@/components/reload";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
+
+      <Reload />
+    </div>
+  );
+}
--- src/components/reload/index.tsx
+++ src/components/reload/index.tsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export default function Reload() {
   return (
     <div className="flex justify-center items-center h-[70vh] bg-gray-200">
       <div className="loader">
