70da36478f9332a2f20fb7f820abc155cefec240
Jerbesson Silva da Costa <jerbessonc@gmail.com>
2025-05-22 14:04:12 -0300

    feat: adição cards usando componente com valores e rótulo;

--- src/app/modulos/dashboards/organizacional/components/card.tsx
+++ src/app/modulos/dashboards/organizacional/components/card.tsx
@@ -1,13 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import { Cairo } from "next/font/google";
-
-interface Empresa {
-  nome: string;
-  endereco: string;
-  // outros campos, conforme necessário
-}
 
 const cairo = Cairo({
   weight: ["500", "600", "700"],
@@ -16,28 +9,24 @@ const cairo = Cairo({
 
 interface CardProps {
   title: string;
-  value: number | Empresa[]; // Agora aceitamos 'number' ou 'Empresa[]'
-  icon: string;
+  value: string; // Value is now a string to accommodate formatted currency
   onClick?: () => void;
 }
 
-export default function Card({ title, value, icon, onClick }: CardProps) {
+export default function Card({ title, value, onClick }: CardProps) {
   return (
     <div
-      className="cursor-pointer py-2 px-4 flex flex-row justify-between items-center w-full h-20 bg-white rounded-md shadow-md"
+      className="cursor-pointer py-2 px-4 flex flex-row items-center w-full h-20 bg-white rounded-md shadow-md"
       onClick={onClick}
     >
-      <div className="py-2 flex flex-col items-start justify-between">
+      <div className="py-2 flex flex-col items-start justify-center flex-grow">
         <span
-          className={`text-base  ${cairo.className} text-left text-gray-500`}
+          className={`text-sm ${cairo.className} text-left text-gray-500`}
         >
           {title}
         </span>
-        <span className={`text-xl ${cairo.className} block`}>
-          {Array.isArray(value) ? value.length : value}
-        </span>
+        <span className={`text-lg ${cairo.className} block`}>{value}</span>
       </div>
-      <Image src={icon} alt={title} width={40} height={40} />
     </div>
   );
 }
--- src/app/modulos/dashboards/organizacional/page.tsx
+++ src/app/modulos/dashboards/organizacional/page.tsx
@@ -1,6 +1,7 @@
 import { Cairo } from "next/font/google";
 import SelecaoIndicadores from "./components/SelecaoIndicadores";
 import SecaoFiltros from "./components/SecaoFiltros";
+import Card from "./components/card";
 
 const cairo = Cairo({
   weight: ["500", "600", "700"],
@@ -8,6 +9,15 @@ const cairo = Cairo({
 });
 
 export default function DashboardOrganizacional() {
+  const cardsData = [
+    { title: "Proventos", value: "R$ 5.811.200,00" },
+    { title: "Descontos", value: "-R$ 1.470.700,00" },
+    { title: "Líquido", value: "R$ 4.340.600,00" },
+    { title: "Custo Total Estimado", value: "R$ 6.452.500,00" },
+    { title: "Custo Médio Mensal", value: "R$ 2.300,00" },
+    { title: "Receita Média Mensal", value: "R$ 14.000,00" },
+  ];
+
   return (
     <div className="bg-[#f7f7f8] min-h-screen">
       <div className="h-auto flex flex-col items-start p-4 gap-4 border-b border-black/10 bg-gray-100">
@@ -15,6 +25,13 @@ export default function DashboardOrganizacional() {
         <SecaoFiltros />
       </div>
       <div className="p-4">
+        <div className="grid grid-cols-6 gap-4">
+          {" "}
+          {/* Container for the cards */}
+          {cardsData.map((card, index) => (
+            <Card key={index} title={card.title} value={card.value} />
+          ))}
+        </div>
       </div>
     </div>
   );
