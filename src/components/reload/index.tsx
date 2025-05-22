0a2cfbdbdbad311eef106642cd9b7af2cb23976f
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-22 14:18:42 -0300

    Atividades Usuários finalizado e testado

--- src/components/reload/index.tsx
+++ src/components/reload/index.tsx
@@ -1,6 +1,6 @@
 export default function Reload() {
   return (
-    <div className="flex justify-center items-center h-[70vh] bg-gray-200">
+    <div className="flex justify-center items-center h-[100vh] bg-gray-200">
       <div className="loader">
         <div className="loader-square"></div>
         <div className="loader-square"></div>
