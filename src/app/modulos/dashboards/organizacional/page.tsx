65e69480e8ffdec43a19d5c1d17f6dad1e62575b
Jerbesson Silva da Costa <jerbessonc@gmail.com>
2025-05-23 10:06:34 -0300

    fix: posicionamento icones de evolução

--- src/app/modulos/dashboards/organizacional/page.tsx
+++ src/app/modulos/dashboards/organizacional/page.tsx
@@ -111,7 +111,7 @@ export default function DashboardOrganizacional() {
           <div className="w-1/2 bg-white rounded-lg h-[627px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
             
-            <div className="flex justify-between items-center pt-[14px] px-5">
+            <div className="flex justify-between items-start pt-[14px] px-5"> {/* Changed items-center to items-start */}
               <div className="flex-grow overflow-hidden mr-3"> 
                 <div title="Evolução" className={`text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap overflow-hidden text-ellipsis`}>Evolução</div>
                 <div className={`text-black text-xs font-light leading-normal ${cairo.className}`}>{kpiSelecionado}</div>
@@ -152,7 +152,7 @@ export default function DashboardOrganizacional() {
           <div className="w-1/2 bg-white rounded-lg h-[627px] border border-neutral-700 relative overflow-hidden"> 
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
             
-            <div className="flex justify-between items-center pt-[14px] px-5">
+            <div className="flex justify-between items-center pt-[14px] px-5"> {/* This one remains items-center */}
               <div title="Valor Por Grupo e Evento" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Grupo e Evento</div>
               <div className="flex items-center gap-2 flex-shrink-0"> 
                 {sectionIcons.map((icon, index) => {
@@ -192,7 +192,7 @@ export default function DashboardOrganizacional() {
           {/* Section 1: Dissídio */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className="flex justify-between items-center pt-[14px] px-5">
+            <div className="flex justify-between items-center pt-[14px] px-5"> {/* This one remains items-center */}
               <div title="Dissídio" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Dissídio</div>
               <div className="flex items-center gap-2 flex-shrink-0"> 
                 {sectionIcons.map((icon, index) => {
@@ -230,7 +230,7 @@ export default function DashboardOrganizacional() {
           {/* Section 2: Valor Por Tipo de Pessoa e Vínculo */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className="flex justify-between items-center pt-[14px] px-5">
+            <div className="flex justify-between items-center pt-[14px] px-5"> {/* This one remains items-center */}
               <div title="Valor Por Tipo de Pessoa e Vínculo" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Tipo de Pessoa e Vínculo</div>
               <div className="flex items-center gap-2 flex-shrink-0"> 
                 {sectionIcons.map((icon, index) => {
@@ -268,7 +268,7 @@ export default function DashboardOrganizacional() {
           {/* Section 3: Valor Por Tipo de Cálculo e Evento */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className="flex justify-between items-center pt-[14px] px-5">
+            <div className="flex justify-between items-center pt-[14px] px-5"> {/* This one remains items-center */}
               <div title="Valor Por Tipo de Cálculo e Evento" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Tipo de Cálculo e Evento</div>
               <div className="flex items-center gap-2 flex-shrink-0"> 
                 {sectionIcons.map((icon, index) => {
