c2361550dd77681c22cca344ecf9c46bcd3c8f82
Jerbesson Silva da Costa <jerbessonc@gmail.com>
2025-05-23 09:58:35 -0300

    feat: título retruncado em seções pequenas

--- src/app/modulos/dashboards/organizacional/page.tsx
+++ src/app/modulos/dashboards/organizacional/page.tsx
@@ -109,88 +109,79 @@ export default function DashboardOrganizacional() {
         <div className="mt-4 flex flex-row gap-4">
           {/* Div da Esquerda (Evolução Chart Card) */}
           <div className="w-1/2 bg-white rounded-lg h-[627px] border border-neutral-700 relative overflow-hidden">
-            {/* Vertical Bar from Figma */}
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
             
-            {/* Evolução Title from Figma */}
-            <div className={`left-[20px] top-[14px] absolute text-black text-xl font-semibold leading-normal ${cairo.className}`}>Evolução</div>
-            
-            {/* Subtitle (Dynamic KPI) from Figma */}
-            <div className={`left-[20px] top-[41px] absolute text-black text-xs font-light leading-normal ${cairo.className}`}>{kpiSelecionado}</div>
-
-            {/* Icons in top-right corner */}
-            <div className="absolute top-[14px] right-4 flex items-center gap-2"> {/* Changed gap-4 to gap-2 */}
-              {sectionIcons.map((icon, index) => {
-                let iconWidth = 16; 
-                let iconHeight = 16; 
-
-                if (icon.adjustSize) {
-                  iconWidth = 14; 
-                  iconHeight = icon.src.includes("lay") ? 16 : 14; 
-                }
-                
-                return (
-                  <React.Fragment key={`left-icon-fragment-${index}`}>
-                    <Image
-                      key={`left-${index}`}
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={iconWidth} 
-                      height={iconHeight} 
-                      className="cursor-pointer"
-                    />
-                    {icon.src === "/assets/icons/icon-hierarchy.svg" && (
-                      <div className="w-px h-5 bg-neutral-300"></div> 
-                    )}
-                  </React.Fragment>
-                );
-              })}
+            <div className="flex justify-between items-center pt-[14px] px-5">
+              <div className="flex-grow overflow-hidden mr-3"> {/* Added flex-grow, overflow-hidden, mr-3 */}
+                <div title="Evolução" className={`text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap overflow-hidden text-ellipsis`}>Evolução</div>
+                <div className={`text-black text-xs font-light leading-normal ${cairo.className}`}>{kpiSelecionado}</div>
+              </div>
+              <div className="flex items-center gap-2 flex-shrink-0"> {/* Added flex-shrink-0 */}
+                {sectionIcons.map((icon, index) => {
+                  let iconWidth = 16; 
+                  let iconHeight = 16; 
+                  if (icon.adjustSize) {
+                    iconWidth = 14; 
+                    iconHeight = icon.src.includes("lay") ? 16 : 14; 
+                  }
+                  return (
+                    <React.Fragment key={`left-icon-fragment-${index}`}>
+                      <Image
+                        key={`left-${index}`}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={iconWidth} 
+                        height={iconHeight} 
+                        className="cursor-pointer"
+                      />
+                      {icon.src === "/assets/icons/icon-hierarchy.svg" && (
+                        <div className="w-px h-5 bg-neutral-300"></div> 
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
             </div>
-
-            {/* Chart Area Placeholder based on Figma */}
-            <div className="w-full h-[537px] px-4 pt-0 pb-4 left-0 top-[90px] absolute bg-white">
+            
+            <div className="w-full h-[537px] px-4 pt-0 pb-4 left-0 top-[90px] absolute bg-white"> 
               <EvolucaoChart data={processedEvolucaoChartData} kpiName={kpiSelecionado} />
             </div>
           </div>
 
           {/* Div da Direita (Valor Por Grupo Chart Card) */}
-          <div className="w-1/2 bg-white rounded-lg h-[627px] border border-neutral-700 relative overflow-hidden p-0">
-            {/* Vertical Bar - same styling as left section */}
+          <div className="w-1/2 bg-white rounded-lg h-[627px] border border-neutral-700 relative overflow-hidden"> 
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
             
-            {/* Title for Right Section - same styling as left section's title */}
-            <div className={`left-[20px] top-[14px] absolute text-black text-xl font-semibold leading-normal ${cairo.className}`}>Valor Por Grupo e Evento</div>
-            
-            {/* Icons in top-right corner */}
-            <div className="absolute top-[14px] right-4 flex items-center gap-2"> {/* Changed gap-4 to gap-2 */}
-              {sectionIcons.map((icon, index) => {
-                let iconWidth = 16; 
-                let iconHeight = 16; 
-
-                if (icon.adjustSize) {
-                  iconWidth = 14; 
-                  iconHeight = icon.src.includes("lay") ? 16 : 14; 
-                }
-
-                return (
-                  <React.Fragment key={`right-icon-fragment-${index}`}>
-                    <Image
-                      key={`right-${index}`}
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={iconWidth}
-                      height={iconHeight}
-                      className="cursor-pointer"
-                    />
-                    {icon.src === "/assets/icons/icon-hierarchy.svg" && (
-                      <div className="w-px h-5 bg-neutral-300"></div> 
-                    )}
-                  </React.Fragment>
-                );
-              })}
+            <div className="flex justify-between items-center pt-[14px] px-5">
+              <div title="Valor Por Grupo e Evento" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Grupo e Evento</div> {/* Added title, flex-grow, overflow-hidden, mr-3, text-ellipsis */}
+              <div className="flex items-center gap-2 flex-shrink-0"> {/* Added flex-shrink-0 */}
+                {sectionIcons.map((icon, index) => {
+                  let iconWidth = 16; 
+                  let iconHeight = 16; 
+                  if (icon.adjustSize) {
+                    iconWidth = 14; 
+                    iconHeight = icon.src.includes("lay") ? 16 : 14; 
+                  }
+                  return (
+                    <React.Fragment key={`right-icon-fragment-${index}`}>
+                      <Image
+                        key={`right-${index}`}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={iconWidth}
+                        height={iconHeight}
+                        className="cursor-pointer"
+                      />
+                      {icon.src === "/assets/icons/icon-hierarchy.svg" && (
+                        <div className="w-px h-5 bg-neutral-300"></div> 
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
             </div>
             
-            <div className="w-full h-[537px] px-4 pt-0 pb-4 left-0 top-[90px] absolute bg-white">
+            <div className="w-full h-[537px] px-4 pt-0 pb-4 left-0 top-[60px] absolute bg-white"> 
               <ValorPorGrupoChart data={valorPorGrupoData} />
             </div>
           </div>
@@ -201,33 +192,35 @@ export default function DashboardOrganizacional() {
           {/* Section 1: Dissídio */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className={`left-[20px] top-[14px] absolute text-black text-xl font-semibold leading-normal ${cairo.className}`}>Dissídio</div>
-            <div className="absolute top-[14px] right-4 flex items-center gap-2"> {/* Changed gap-4 to gap-2 */}
-              {sectionIcons.map((icon, index) => {
-                 let iconWidth = 16; 
-                 let iconHeight = 16; 
-                 if (icon.adjustSize) {
-                   iconWidth = 14; 
-                   iconHeight = icon.src.includes("lay") ? 16 : 14; 
-                 }
-                 return (
-                  <React.Fragment key={`s1-icon-fragment-${index}`}>
-                    <Image
-                      key={`s1-icon-${index}`}
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={iconWidth} 
-                      height={iconHeight} 
-                      className="cursor-pointer"
-                    />
-                    {icon.src === "/assets/icons/icon-hierarchy.svg" && (
-                      <div className="w-px h-5 bg-neutral-300"></div>
-                    )}
-                  </React.Fragment>
-                );
-              })}
+            <div className="flex justify-between items-center pt-[14px] px-5">
+              <div title="Dissídio" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Dissídio</div> {/* Added title, flex-grow, overflow-hidden, mr-3, text-ellipsis */}
+              <div className="flex items-center gap-2 flex-shrink-0"> {/* Added flex-shrink-0 */}
+                {sectionIcons.map((icon, index) => {
+                   let iconWidth = 16; 
+                   let iconHeight = 16; 
+                   if (icon.adjustSize) {
+                     iconWidth = 14; 
+                     iconHeight = icon.src.includes("lay") ? 16 : 14; 
+                   }
+                   return (
+                    <React.Fragment key={`s1-icon-fragment-${index}`}>
+                      <Image
+                        key={`s1-icon-${index}`}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={iconWidth} 
+                        height={iconHeight} 
+                        className="cursor-pointer"
+                      />
+                      {icon.src === "/assets/icons/icon-hierarchy.svg" && (
+                        <div className="w-px h-5 bg-neutral-300"></div>
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
             </div>
-            <div className="w-full h-[calc(489px-60px)] px-4 pt-2 pb-4 left-0 top-[60px] absolute bg-white">
+            <div className="w-full h-[calc(489px-50px)] px-4 pt-2 pb-4 left-0 top-[50px] absolute bg-white"> 
               <div className="w-full h-full border border-dashed border-gray-300 flex items-center justify-center">
                 <span className="text-gray-400">Dissídio Content Area</span>
               </div>
@@ -237,33 +230,35 @@ export default function DashboardOrganizacional() {
           {/* Section 2: Valor Por Tipo de Pessoa e Vínculo */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className={`left-[20px] top-[14px] absolute text-black text-xl font-semibold leading-normal ${cairo.className}`}>Valor Por Tipo de Pessoa e Vínculo</div>
-             <div className="absolute top-[14px] right-4 flex items-center gap-2"> {/* Changed gap-4 to gap-2 */}
-              {sectionIcons.map((icon, index) => {
-                let iconWidth = 16; 
-                let iconHeight = 16; 
-                if (icon.adjustSize) {
-                  iconWidth = 14; 
-                  iconHeight = icon.src.includes("lay") ? 16 : 14;
-                }
-                return (
-                  <React.Fragment key={`s2-icon-fragment-${index}`}>
-                    <Image
-                      key={`s2-icon-${index}`}
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={iconWidth} 
-                      height={iconHeight} 
-                      className="cursor-pointer"
-                    />
-                    {icon.src === "/assets/icons/icon-hierarchy.svg" && (
-                      <div className="w-px h-5 bg-neutral-300"></div>
-                    )}
-                  </React.Fragment>
-                );
-              })}
+            <div className="flex justify-between items-center pt-[14px] px-5">
+              <div title="Valor Por Tipo de Pessoa e Vínculo" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Tipo de Pessoa e Vínculo</div> {/* Added title, flex-grow, overflow-hidden, mr-3, text-ellipsis */}
+              <div className="flex items-center gap-2 flex-shrink-0"> {/* Added flex-shrink-0 */}
+                {sectionIcons.map((icon, index) => {
+                  let iconWidth = 16; 
+                  let iconHeight = 16; 
+                  if (icon.adjustSize) {
+                    iconWidth = 14; 
+                    iconHeight = icon.src.includes("lay") ? 16 : 14;
+                  }
+                  return (
+                    <React.Fragment key={`s2-icon-fragment-${index}`}>
+                      <Image
+                        key={`s2-icon-${index}`}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={iconWidth} 
+                        height={iconHeight} 
+                        className="cursor-pointer"
+                      />
+                      {icon.src === "/assets/icons/icon-hierarchy.svg" && (
+                        <div className="w-px h-5 bg-neutral-300"></div>
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
             </div>
-            <div className="w-full h-[calc(489px-60px)] px-4 pt-2 pb-4 left-0 top-[60px] absolute bg-white">
+            <div className="w-full h-[calc(489px-50px)] px-4 pt-2 pb-4 left-0 top-[50px] absolute bg-white"> 
               <div className="w-full h-full border border-dashed border-gray-300 flex items-center justify-center">
                 <span className="text-gray-400">Pessoa e Vínculo Chart Area</span>
               </div>
@@ -273,33 +268,35 @@ export default function DashboardOrganizacional() {
           {/* Section 3: Valor Por Tipo de Cálculo e Evento */}
           <div className="flex-1 bg-white rounded-lg h-[489px] border border-neutral-700 relative overflow-hidden">
             <div className="w-6 h-0 left-[10px] top-[17px] absolute origin-top-left rotate-90 bg-zinc-300 outline-1 outline-offset-[-0.50px] outline-neutral-700"></div>
-            <div className={`left-[20px] top-[14px] absolute text-black text-xl font-semibold leading-normal ${cairo.className}`}>Valor Por Tipo de Cálculo e Evento</div>
-            <div className="absolute top-[14px] right-4 flex items-center gap-2"> {/* Changed gap-4 to gap-2 */}
-              {sectionIcons.map((icon, index) => {
-                let iconWidth = 16; 
-                let iconHeight = 16; 
-                if (icon.adjustSize) {
-                  iconWidth = 14; 
-                  iconHeight = icon.src.includes("lay") ? 16 : 14;
-                }
-                return (
-                  <React.Fragment key={`s3-icon-fragment-${index}`}>
-                    <Image
-                      key={`s3-icon-${index}`}
-                      src={icon.src}
-                      alt={icon.alt}
-                      width={iconWidth} 
-                      height={iconHeight} 
-                      className="cursor-pointer"
-                    />
-                    {icon.src === "/assets/icons/icon-hierarchy.svg" && (
-                      <div className="w-px h-5 bg-neutral-300"></div>
-                    )}
-                  </React.Fragment>
-                );
-              })}
+            <div className="flex justify-between items-center pt-[14px] px-5">
+              <div title="Valor Por Tipo de Cálculo e Evento" className={`flex-grow overflow-hidden mr-3 text-black text-xl font-semibold leading-normal ${cairo.className} whitespace-nowrap text-ellipsis`}>Valor Por Tipo de Cálculo e Evento</div> {/* Added title, flex-grow, overflow-hidden, mr-3, text-ellipsis */}
+              <div className="flex items-center gap-2 flex-shrink-0"> {/* Added flex-shrink-0 */}
+                {sectionIcons.map((icon, index) => {
+                  let iconWidth = 16; 
+                  let iconHeight = 16; 
+                  if (icon.adjustSize) {
+                    iconWidth = 14; 
+                    iconHeight = icon.src.includes("lay") ? 16 : 14;
+                  }
+                  return (
+                    <React.Fragment key={`s3-icon-fragment-${index}`}>
+                      <Image
+                        key={`s3-icon-${index}`}
+                        src={icon.src}
+                        alt={icon.alt}
+                        width={iconWidth} 
+                        height={iconHeight} 
+                        className="cursor-pointer"
+                      />
+                      {icon.src === "/assets/icons/icon-hierarchy.svg" && (
+                        <div className="w-px h-5 bg-neutral-300"></div>
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </div>
             </div>
-            <div className="w-full h-[calc(489px-60px)] px-4 pt-2 pb-4 left-0 top-[60px] absolute bg-white">
+            <div className="w-full h-[calc(489px-50px)] px-4 pt-2 pb-4 left-0 top-[50px] absolute bg-white"> 
               <div className="w-full h-full border border-dashed border-gray-300 flex items-center justify-center">
                 <span className="text-gray-400">Cálculo e Evento Chart Area</span>
               </div>
