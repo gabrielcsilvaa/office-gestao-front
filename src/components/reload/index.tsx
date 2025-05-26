31804ed8de1fc9f0b9f5b666140f19c5df620cdf
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-05-26 08:38:34 -0300

    Correção loading

--- src/components/reload/index.tsx
+++ src/components/reload/index.tsx
@@ -1,6 +1,6 @@
 export default function Reload() {
   return (
-    <div className="flex justify-center items-center h-[100vh] bg-gray-200">
+    <div className="flex justify-center items-center h-[calc(95vh-85px)] bg-gray-200">
       <div className="loader">
         <div className="loader-square"></div>
         <div className="loader-square"></div>
